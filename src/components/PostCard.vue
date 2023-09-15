e7bb0a0eb94ed6546b1bdc4455a04c7ba5eeccf6
AxelArciniega <axelarciniega12@icloud.com>
2023-09-15 12:03:25 -0600

    got the card ready to go

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,19 @@
 <template>
-    🐄{{ post.body }}
+    <div v-if="post" class="card elevation-5 selectable my-3">
+        <section class="row">
+            <div class="col-5">
+                <h3> <img class="profile-pic" :src="post.creator.picture" alt="">{{ post.creator.name }}</h3>
+                <Span>{{ post.createdAt.toLocaleTimeString() }}</Span>
+            </div>
+            <div class="text-center mt-3">
+                <i>{{ post.body }}</i>
+            </div>
+            <div class="col-12 d-flex justify-content-center">
+                <img class="post-picture" :src="post.imgUrl" alt="">
+            </div>
+            
+        </section>
+    </div>
 </template>
 
 <script>
@@ -15,4 +29,16 @@ setup() {
 
 
 <style>
+
+.post-picture{
+    height: 41vh;
+    width: 55vh;
+}
+
+.profile-pic{
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+}
+
 </style>
--- src/models/Post.js
+++ src/models/Post.js
@@ -4,9 +4,11 @@ export class Post{
     constructor(data){
         this.id = data.id
         this.body = data.body
-        this.imgUrl = data.imgUrl
+        this.imgUrl = data.imgUrl || ''
         this.creatorId = data.creatorId
         this.likeIds = data.likeIds
         this.posts = data.posts
+        this.creator = data.creator
+        this.createdAt = new Date(data.createdAt)
     }
 }
